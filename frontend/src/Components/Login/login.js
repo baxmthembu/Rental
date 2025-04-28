a817306f27af8c1b0a434f291f142a4160cc587d
Bongumusa <bmthembu2002@gmail.com>
2025-04-28 16:39:06 +0200

    porsche

--- frontend/src/Components/Login/login.js
+++ frontend/src/Components/Login/login.js
@@ -32,7 +32,7 @@ const Login = () => {
         } else if (formData.name.length < 3) {
             errors.name = 'Name must be at least 3 characters';
         }else if (!nameRegex.test(formData.name)) {
-            errors.name = 'Name can only contain letters and numbers';
+            errors.name = 'Name can only contain letters';
         }
 
         if (!formData.password) {
@@ -128,7 +128,7 @@ const Login = () => {
                     <div className="form-container">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="icon"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
                         <input value={formData.password} onChange={handleChange} type="password" name="password" placeholder="Enter password" required/><br /><br />
-                        {errors.password && <span className="error-message">{errors.password}</span>}
+                        {errors.password && <span className="error-message2">{errors.password}</span>}
                     </div>
                     <ReCAPTCHA
                         sitekey="6Lc3CKYnAAAAAHjblBln1V7QStAE_H6kD5tYuMPl"
