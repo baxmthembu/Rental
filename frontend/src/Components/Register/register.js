ae2464a19e07b0b05e52d0b032c4df93d0e8b8d0
Bongumusa <bmthembu2002@gmail.com>
2025-04-28 16:10:53 +0200

    panda

--- frontend/src/Components/Register/register.js
+++ frontend/src/Components/Register/register.js
@@ -33,7 +33,7 @@ const Register = () => {
         if(validationResponse.isValid){
             try {
                 const { password2, ...dataToSend } = formData;
-                const response = await Axios.post(/*'http://localhost:3001/register'*/ `${process.env.REACT_APP_API_URL}/register`, dataToSend);
+                const response = await Axios.post(`${process.env.REACT_APP_API_URL}/register`, dataToSend);
 
                 if(response.status === 200){
                     console.log('Register Successful')
