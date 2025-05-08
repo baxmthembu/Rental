f36e8bc4f452dc8b761e436f189a7361aa338e4d
Bongumusa <bmthembu2002@gmail.com>
2025-05-08 17:15:54 +0200

    errorMessage

--- frontend/src/Components/Register/register.js
+++ frontend/src/Components/Register/register.js
@@ -72,10 +72,8 @@ const Register = () => {
                     <div className="form-container">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="icon"><path d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z"/></svg>
                         <input value={formData.password} onChange={handleChange} type="password" name="password" placeholder="Enter password" required autoComplete="off" />
+                        {errorMessage && <div className="error-message">{errorMessage}</div>}
                     </div>
-                    {/*<div className="form-container">
-                        <p><input type="checkbox" className="icon" style={{cursor: 'pointer'}} required/>I agree all statements in <a href="/home" className="a">Terms of service</a></p>
-                    </div>*/}
                     <div className="button">
                         <button type="submit" className="submit">Submit</button>
                         <p>Already have account? <Link to='/' className="register-link">Sign in</Link></p>
