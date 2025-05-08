3760ac7a335a03d23e954451f33e981211ac0f6f
Bongumusa <bmthembu2002@gmail.com>
2025-05-08 19:09:14 +0200

    amalanga awafani

--- frontend/src/Components/Login/login.js
+++ frontend/src/Components/Login/login.js
@@ -119,7 +119,7 @@ const Login = () => {
                     <h1 className="header">Sign in</h1>
                     <div className="form-container">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="icon"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>
-                        <input value={formData.name} onChange={handleChange} type="email" name="email" placeholder="Enter email" required /><br />
+                        <input value={formData.email} onChange={handleChange} type="email" name="email" placeholder="Enter email" required /><br />
                         {errors.email && <span className="error-message">{errors.email}</span>}
                     </div>
                     <div className="form-container">
