b793d26fa9aadad3913ed20af3d592332e51d5cf
Bongumusa <bmthembu2002@gmail.com>
2025-05-09 17:46:25 +0200

    Login email icon

--- frontend/src/Components/Login/login.js
+++ frontend/src/Components/Login/login.js
@@ -119,7 +119,7 @@ const Login = () => {
                     </div>
                     <h1 className="header">Sign in</h1>
                     <div className="form-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="icon"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="icon"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
                         <input value={formData.email} onChange={handleChange} type="email" name="email" placeholder="Enter email" required /><br />
                         {errors.email && <span className="error-message">{errors.email}</span>}
                     </div>
