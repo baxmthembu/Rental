e2d3570eb6eb3c0d2f0de797460a04996378d421
Bongumusa <bmthembu2002@gmail.com>
2025-09-13 11:16:55 +0200

    roddy

--- frontend/src/routes/AuthOutlet.js
+++ frontend/src/routes/AuthOutlet.js
@@ -5,12 +5,18 @@ import { useAuth } from "../provider/authProvider";
 const AuthOutlet = () => {
     const { user, loading } = useAuth();
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (!user) {
-        return <Navigate to="/login" replace />;
+     if (loading) {
+        return (
+            <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
+                <div className="flex flex-col items-center">
+                    <div className="relative">
+                        <div className="w-16 h-16 border-4 border-blue-100 rounded-full"></div>
+                        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full absolute top-0 left-0 animate-spin"></div>
+                    </div>
+                    <p className="mt-4 text-lg font-medium text-gray-700">Loading...</p>
+                </div>
+            </div>
+        );
     }
 
     /*return (
